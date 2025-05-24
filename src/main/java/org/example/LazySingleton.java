1cb0fd8a26d761547625c0403045896101ea8274
linulg20 <129095672+linulg20@users.noreply.github.com>
2025-05-24 17:04:45 -0500

    LazySingleton added missing and corrected return statement and javadoc comments

--- src/main/java/org/example/LazySingleton.java
+++ src/main/java/org/example/LazySingleton.java
@@ -1,19 +1,38 @@
 package org.example;
+/** i shud make an array size of 3*/
+/** keeps the lazysingleton variables*/
 public class LazySingleton {
-    private static final LazySingleton[] singleton = new LazySingleton[3];
-
-
-    private LazySingleton(int i) {
-        this.num = i;
-        System.out.println("created lazy singleton number" + i);
+    private static final LazySingleton[] LAZY_SINGLETONS = new LazySingleton[3];
+    private final int num;
+    /**
+     * this constructor is priv so nobody accesses it or changes it
+     * prints a msg when the singleton is made
+     * @param id is a number to know the singleton intance
+     */
+    private LazySingleton(int id) {
+        this.num = id;
+        System.out.println("LazySingleton " + id + " instantiated");
     }
-    public static LazySingleton getInstance(int i) {
-        if (i < 0 || i >= singleton.length) {
+
+    /**
+     * picks one of the three singletons n if it doesn’t exist, it will make it
+     * @param index Which one you want from 0 to 2
+     * @return The singleton at that location
+     * @throws IllegalArgumentException if its not 0 to 2.
+     */
+    public static LazySingleton getInstance(int index) {
+        if (index < 0 || index >= LAZY_SINGLETONS.length) {
             throw new IllegalArgumentException("Index must be between 0 and 2");
         }
-        if (singleton[i] == null) {
-            singleton[i] = new LazySingleton(i);
+        if (LAZY_SINGLETONS[index] == null) {
+            LAZY_SINGLETONS[index] = new LazySingleton(index);
         }
+        return LAZY_SINGLETONS[index];
+    }
+    /**
+     * See which one this is.
+     * @return The instance’s number
+     */
     public int getNum() {
         return num;
     }
