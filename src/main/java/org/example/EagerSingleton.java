de42d859f3db5b75c2322f1ac33c9c4d39399166
linulg20 <129095672+linulg20@users.noreply.github.com>
2025-05-24 15:53:29 -0500

    EagerSingleton 1st work on constructing the class

--- src/main/java/org/example/EagerSingleton.java
+++ src/main/java/org/example/EagerSingleton.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class EagerSingleton {
-}
+
+    private static final EagerSingleton[] instance = new EagerSingleton[3];
+
+    static {
+        for (int i = 0; i < instance.length; i++) {
+            instance[i] = new EagerSingleton(i);
+            System.out.println("EagerSingleton " + i + " example");
+        }
+    }
+
+
+    public static EagerSingleton getInstance(int index) {
+        if (index < 0 || index >= instance.length) {
+            throw new IllegalArgumentException("number must be from 0 and 2");
+        }
+        return instance[index];
+    }
+
--- src/main/java/org/example/LazySingleton.java
+++ src/main/java/org/example/LazySingleton.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class LazySingleton {
+    public static LazySingleton getInstance(int i) {
+    }
+
+    public String getId() {
+    }
 }
