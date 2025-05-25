e8da82483bdc75ff07df493aacb130d15eb96866
linulg20 <129095672+linulg20@users.noreply.github.com>
2025-05-24 19:56:05 -0500

    making sky animal interface

---
+++ src/main/java/org/example/SkyAnimal.java
@@ -0,0 +1,7 @@
+package org.example;
+/** making the sky animal section for flying animals with their
+ * names and how they fly */
+public interface SkyAnimal {
+    String getName();
+    String getFlying();
+}
