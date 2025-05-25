43e784ce04ad5e9e862d2e8d0c8e98ba9502c5ce
linulg20 <129095672+linulg20@users.noreply.github.com>
2025-05-24 21:39:30 -0500

    I did a lot. i made 2 more animals and made a test case to run if it works, added javadoc comments that is required

--- src/main/java/org/example/SeaAnimal.java
+++ src/main/java/org/example/SeaAnimal.java
@@ -1,5 +1,5 @@
 package org.example;
-/** interface for the sea animals that can swim */
+/** interface for the sea animals that can transport*/
 public interface SeaAnimal {
     String getName();
     String getSwimming();
