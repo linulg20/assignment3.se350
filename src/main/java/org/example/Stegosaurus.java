cab421c62e6a4dee3690cf9bfd586e57afee9aa0
linulg20 <129095672+linulg20@users.noreply.github.com>
2025-05-24 20:43:48 -0500

    stegosaurus class editing it

---
+++ src/main/java/org/example/Stegosaurus.java
@@ -0,0 +1,5 @@
+package org.example;
+public class Stegosaurus implements LandAnimal {
+    @Override public String getName()    { return "Stegosaurus"; }
+    @Override public String getWalking() { return "stomping"; }
+}
