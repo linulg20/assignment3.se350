34b0f82bf1324e67c60a979482a62c6d76368c00
linulg20 <129095672+linulg20@users.noreply.github.com>
2025-05-24 19:39:24 -0500

    making land animal interface

---
+++ src/main/java/org/example/LandAnimal.java
@@ -0,0 +1,7 @@
+package org.example;
+/** inteerface for land animals that identifies the name of the animal
+ * and the style of walking */
+public interface LandAnimal {
+    String getName();
+    String getWalking();
+}
